75d4205afe78e7728be117d5a8ff0befdd4733a8
James Hochadel <james.hochadel@gmail.com>
2016-02-09 13:03:30 -0500

    Added interpreter file and basic README

---
+++ interpreter.scm
@@ -0,0 +1,10 @@
+; EECS 345 Class Project 1
+; James Hochadel and Andrew Marmorstein
+(load "simpleParser.scm")
+
+(define interpret
+  (lambda (filename)
+  	; Feed file into parser
+  	; Evaluate the parse tree returned by parser
+  	; Return the appropriate value
+  ))
