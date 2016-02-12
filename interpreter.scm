eb2a95718b46baab3ef572ba60250169a59412d7
Andrew <agm67@case.edu>
2016-02-11 22:07:10 -0500

    added the lookup method

--- interpreter.scm
+++ interpreter.scm
@@ -8,3 +8,12 @@
   	; Evaluate the parse tree returned by parser
   	; Return the appropriate value
   ))
+
+;lookup gets the value for a given variable
+;takes a variable name and the state and returns the value of that variable
+(define lookup
+  (lambda (var s)
+    (cond
+      ((null? (car s)) 'error)
+      ((eq? (caar s) var) (caadr s))
+      (else (lookup var (cons (cdar s) (cons (cdadr s) '())))))))
